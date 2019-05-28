6fb346a6761090240fdbce072c15ce3898d1747e
gregortaube <gregjotau@gmail.com>
2019-05-28 21:37:31 +0200

    working version barcode generator app

---
+++ LabelSheetGenerator.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+from reportlab.graphics import renderPDF
+from reportlab.graphics.barcode.eanbc import Ean13BarcodeWidget
+from reportlab.graphics.shapes import Drawing, String
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfgen.canvas import Canvas
+
+"""
+Adjust pagesize, number of labels, barcode size and
+positions of barcode and description to your needs.
+"""
+PAGESIZE = A4
+NUM_LABELS_X = 2
+NUM_LABELS_Y = 8
+BAR_WIDTH = 1.5
+BAR_HEIGHT = 41.0
+TEXT_Y = 95
+BARCODE_Y = 17
+
+LABEL_WIDTH = PAGESIZE[0] / NUM_LABELS_X
+LABEL_HEIGHT = PAGESIZE[1] / NUM_LABELS_Y
+SHEET_TOP = PAGESIZE[1]
+
+STICKER_ELEMENT_Y_DISTANCE = 15
+COLUMN2_X = 180
+
+
+class LabelSheetGenerator:
+
+    def label(self, ean13: str, description: str, sku, color, composition, origin_country) -> Drawing:
+        """
+        Generate a drawing with EAN-13 barcode and descriptive text.
+
+        :param origin_country:
+        :param composition:
+        :param sku:
+        :param color:
+        :param ean13: The EAN-13 Code.
+        :type ean13: str
+        :param description: Short product description.
+        :type description: str
+        :return: Drawing with barcode and description
+        :rtype: Drawing
+        """
+
+        # First column of sticker
+        barcode = Ean13BarcodeWidget(ean13)
+        barcode.barWidth = BAR_WIDTH
+        barcode.barHeight = BAR_HEIGHT
+        x0, y0, bw, bh = barcode.getBounds()
+        x_alignment = 10
+        barcode.x = x_alignment  # center barcode
+        top_element_height = LABEL_HEIGHT / 2
+        barcode.y = top_element_height  # spacing from label bottom (pt)
+
+        product_name = String(x_alignment, top_element_height - STICKER_ELEMENT_Y_DISTANCE,
+                              description, fontName="Helvetica",
+                              fontSize=10)
+        color = String(x_alignment, top_element_height - 2 * STICKER_ELEMENT_Y_DISTANCE,
+                       "Color: {}".format(color),
+                       fontName="Helvetica",
+                       fontSize=10)
+        composition = String(x_alignment, top_element_height - 3 * STICKER_ELEMENT_Y_DISTANCE, "{}".format(composition),
+                             fontName="Helvetica",
+                             fontSize=8)
+
+        # Second column of sticker
+        column2_y_top = LABEL_HEIGHT / 1.3
+        origin_country = String(COLUMN2_X, column2_y_top,
+                                "Made in: {}".format(origin_country), fontName="Helvetica",
+                                fontSize=10)
+        sku_drawing_element = String(COLUMN2_X, column2_y_top - STICKER_ELEMENT_Y_DISTANCE,
+                                     "SKU: {}".format(sku), fontName="Helvetica",
+                                     fontSize=10)
+        size_text = String(COLUMN2_X, column2_y_top - STICKER_ELEMENT_Y_DISTANCE * 2.5,
+                           "Size: ", fontName="Helvetica",
+                           fontSize=10)
+        size = String(COLUMN2_X + 35, column2_y_top - STICKER_ELEMENT_Y_DISTANCE * 2.5, "{}".format(sku.split("-")[-1]),
+                      fontName="Helvetica",
+                      fontSize=15, textAnchor="middle")
+
+        label_drawing = Drawing(LABEL_WIDTH, LABEL_HEIGHT)
+        label_drawing.add(barcode)
+        label_drawing.add(product_name)
+        label_drawing.add(color)
+        label_drawing.add(composition)
+        # Column 2
+        label_drawing.add(origin_country)
+        label_drawing.add(sku_drawing_element)
+        label_drawing.add(size_text)
+        label_drawing.add(size)
+        return label_drawing
+
+    def fill_sheet(self, canvas: Canvas, label_drawing: Drawing):
+        """
+        Simply fill the given ReportLab canvas with label drawings.
+
+        :param canvas: The ReportLab canvas
+        :type canvas: Canvas
+        :param label_drawing: Contains Drawing of configured size
+        :type label_drawing: Drawing
+        """
+        for u in range(0, NUM_LABELS_Y):
+            for i in range(0, NUM_LABELS_X):
+                x = i * LABEL_WIDTH
+                y = SHEET_TOP - LABEL_HEIGHT - u * LABEL_HEIGHT
+                renderPDF.draw(label_drawing, canvas, x, y)
--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfgen.canvas import Canvas
 
 app = Flask(__name__)
+from LabelSheetGenerator import LabelSheetGenerator
+
+app.labelGenerator = LabelSheetGenerator()
 
 
 @app.route('/')
 def index():
     return render_template('index.html', page_title='Famme dev')
+
+
+@app.route('/generate_barcodes')
+def generate_barcodes():
+    product = request.args.get("product")
+    sku = request.args.get("SKU")
+    ean13 = request.args.get("EAN13")
+    color = request.args.get("color")
+    composition = request.args.get("composition")
+    origin = request.args.get("origin")
+    filename = "{}_{}_ean-sticker.pdf".format(sku, ean13)
+    canvas = Canvas(filename,
+                    pagesize=A4)
+
+    sticker = app.labelGenerator.label(ean13,
+                                       product,
+                                       sku,
+                                       color,
+                                       composition,
+                                       origin)
+    app.labelGenerator.fill_sheet(canvas, sticker)
+
+    canvas.save()
+    pdf_out = canvas.getpdfdata()
+    response = make_response(pdf_out)
+    response.headers['Content-Disposition'] = "attachment; filename={}".format(filename)
+    response.mimetype = 'application/pdf'
+    return response
 
 
 if __name__ == '__main__':
